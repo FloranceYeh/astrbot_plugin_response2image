e8f417a058eebb1ef698152b7168074c1baf8247
FloranceYeh <Florance_Sunrise@outlook.com>
2026-06-03 09:20:29 +0800

    优化帮助指令

--- main.py
+++ main.py
@@ -73,14 +73,14 @@ class Response2Image(Star):
     async def r2i_help(self, event: AstrMessageEvent):
         yield self._plain_result(
             event,
-            "r2i-Response2Image\n"
-            "• /r2i img <提示词> [--ref]      自动判断文生图/改图\n"
-            "• /r2i aiimg <提示词>       文生图\n"
-            "• /r2i aiedit <提示词> [--ref]      图生图\n"
-            "• /r2i selfie <提示词> [--ref]      自拍\n"
-            "• /r2i selfie_ref set       发送或引用图片后执行\n"
-            "• /r2i selfie_ref list      查看当前参考图\n"
-            "• /r2i selfie_ref clear     清空命令保存的参考图\n"
+            "Response2Image\n"
+            "• /r2i img <提示词> [--ref]\n    自动判断文生图/改图\n"
+            "• /r2i aiimg <提示词>\n    文生图\n"
+            "• /r2i aiedit <提示词> [--ref]\n    图生图\n"
+            "• /r2i selfie <提示词> [--ref]\n    自拍\n"
+            "• /r2i selfie_ref set\n    发送或引用图片后执行\n"
+            "• /r2i selfie_ref list\n    查看当前参考图\n"
+            "• /r2i selfie_ref clear\n    清空命令保存的参考图\n"
         )
 
     @r2i.command("img")
